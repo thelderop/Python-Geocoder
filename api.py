db18eb10395c52b8eaf1f48a6c08c0010a2e61dc
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-10 10:46:10 -0600

    geocoder assignment

--- api.py
+++ api.py
@@ -7,7 +7,6 @@ destinations = ["The Space Needle",
   "Yosemite National Park",
   "Las Vegas, Nevada",
   "Grand Canyon National Park",
-  "Aspen, Colorado",
   "Mount Rushmore",
   "Yellowstone National Park",
   "Sandpoint, Idaho",
@@ -15,9 +14,6 @@ destinations = ["The Space Needle",
   "Capilano Suspension Bridge"]
 
 for point in destinations:
-  # Get the latitude and longitude from `geocoder`.
   loc = geocoder.arcgis(point)
 
-  # Print out `geopy`'s results.
   print("{0} is located at ({1:.4f}, {2: .4f})".format(point, loc.latlng[0], loc.latlng[1]))
-
